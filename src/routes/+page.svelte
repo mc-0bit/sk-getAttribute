19896b94b7631d33f52c05cc6c1efe4aef461f44
mc_0bit <mc0bit.dev@gmail.com>
2022-12-02 10:09:41 +0100

    issue

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  let visible = true;
+  function toggle() {
+    visible = !visible;
+  }
+</script>
+
+{#if visible}
+  <a href="/" on:click={toggle}>remove</a>
+{:else}
+  <a href="/" on:click={toggle}>add</a>
+{/if}
